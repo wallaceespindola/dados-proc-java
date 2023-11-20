3e8b370867ccb6bd6b61e3f94c5dc22259d1b35b
Wallace ESPINDOLA <wallace.espindola@iba-group.com>
2023-11-20 19:16:53 +0100

    Initial commit

---
+++ src/main/java/com/wtech/App.java
@@ -0,0 +1,13 @@
+package com.wtech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
